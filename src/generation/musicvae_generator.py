85b1f203c411cff81af18b2e789fff7e2f51ddb0
peregrin-tuk <eric.thalhammer@gmx.net>
2021-01-14 21:18:15 +0100

    created conda env.yml, moved requirements and visual midi clone to dependencies; added imports in VAE generator

--- src/generation/musicvae_generator.py
+++ src/generation/musicvae_generator.py
@@ -1,7 +1,14 @@
 from . import AbstractGenerator
+from enum import Enum
+from magenta.models.music_vae import configs
+from magenta.models.music_vae.trained_model import TrainedModel
+
+class Checkpoint(Enum):
+    MEL_2BAR = 0  # cat-mel_2bar_big
+    MEL_16BAR = 1 # hierdec-mel_16bar
 
 class MusicVAEGenerator(AbstractGenerator):
 
-    def __init__(self):
+    def __init__(self, checkpoint = Checkpoint.MEL_2BAR):
         super().__init__()
     
