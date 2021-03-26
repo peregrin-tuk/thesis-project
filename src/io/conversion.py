4c6d9329092e6a1d276c9b92d59aedc032941095
peregrin-tuk <eric.thalhammer@gmx.net>
2021-03-26 18:37:52 +0100

    working implementation of own convert functions betw. pretty_midi and music21

--- src/io/conversion.py
+++ src/io/conversion.py
@@ -3,6 +3,7 @@ import music21
 import mido
 from pretty_midi import PrettyMIDI
 from note_seq import NoteSequence, note_sequence_to_pretty_midi, midi_to_note_sequence
+import pretty_midi
 
 #   PrettyMIDI <-> Music21
 def pretty_midi_to_music21(melody: PrettyMIDI):
@@ -145,7 +146,7 @@ def _patched_from_pretty_midi(midi: PrettyMIDI, resolution: int = None):
 
 
 
-from music21.stream import Score, Part
+from music21.stream import Score, Part, Stream
 from music21.tempo import MetronomeMark
 from music21.pitch import Pitch
 from music21.key import Key
@@ -154,6 +155,9 @@ from music21.note import Note
 
 def _seconds_to_quarterLength(seconds: float, qpm: int):
     return seconds * qpm / 60.0
+
+def _quarterLength_to_seconds(quarterLength: float, qpm: int):
+    return quarterLength * 60.0 / qpm
 
 
 def own_pretty_midi_to_music21(midi: PrettyMIDI):
@@ -204,7 +208,7 @@ def own_pretty_midi_to_music21(midi: PrettyMIDI):
             ts21.offset = _seconds_to_quarterLength(ts.time, qpm)
             part.append(ts21)
 
-        # add Notes to Part
+        # Add Notes to Part
         for note in track.notes:
             note21 = Note(_get_pitch_name(note.pitch))
             note21.quarterLength = _seconds_to_quarterLength(note.get_duration(), qpm)
@@ -219,12 +223,62 @@ def own_pretty_midi_to_music21(midi: PrettyMIDI):
 
 
 
+def own_music21_to_pretty_midi(stream: Stream, resolution: int = 480):
+    '''
+    Note: 
+    - Converts all features that could have previously been converted from PrettyMIDI to a music21 stream.
+      Does not support advanced features of music21 such as note ties etc.
+    - In PrettyMIDI, tempo changes are read-only and cannot be set manually, therefore only the first MetronomeMark is considered for the conversion and the tempo will stay the same for the whole sequence.
 
-# use muspy template
-# make sure time codes / resolution are correct
-# make sure start time is correct
-# write velocity into volume
+    '''
+    # Get initial tempo and create PrettyMIDI object
+    if len(stream.flat.metronomeMarkBoundaries()) > 0:
+        initial_tempo = stream.flat.metronomeMarkBoundaries()[0][2].number
+    else:
+        initial_tempo = 120
 
-# TODO we probably also need a new music21 to pretty midi
+    midi = PrettyMIDI(resolution=resolution, initial_tempo=initial_tempo)
+
+
+    # Tracks
+    for part in stream.parts:
+
+        # Create Instrument
+        if part.instrument.midiProgram is not None:
+            program = part.instrument.midiProgram
+        else:
+            program = 0
+
+        if part.instrument.midiChannel is not None:
+            is_drum = part.instrument.midiChannel == 10
+        else:
+            is_drum = False
+
+        instrument = pretty_midi.Instrument(program, is_drum, part.partName)
+
+
+        # iterate over elements in part
+        for el in part.secondsMap:
+
+            # Add notes to instrument
+            if isinstance(el['element'], Note):
+                note = pretty_midi.Note(el['element'].volume.velocity, int(el['element'].pitch.ps), el['offsetSeconds'], el['endTimeSeconds'])
+                instrument.notes.append(note)
+
+            # Key Signatures
+            elif isinstance(el['element'], music21.key.Key):
+                ks = pretty_midi.KeySignature(el['element'].getTonic().ps, el['offsetSeconds'])
+                midi.key_signature_changes.append(ks)
+
+            # Time Signatures
+            elif isinstance(el['element'], music21.meter.TimeSignature):
+                ts = pretty_midi.TimeSignature(el['element'].numerator, el['element'].denominator, el['offsetSeconds'])
+                midi.time_signature_changes.append(ts)
+
+        midi.instruments.append(instrument)
+
+    return midi
+
+
 # CHECK + the notesequence to music21 methods should then use this internally
 # => muspy will only be left in mido to pretty_midi conversion and in reference data preparation
